e5175d64c987aad874d992465479f60182287041
Max Holder <mxhold@gmail.com>
2018-11-01 22:50:28 -0700

    Refactor to remove nesting

--- domain/src/convertIngredient.js
+++ domain/src/convertIngredient.js
@@ -11,24 +11,6 @@ var convertIngredientError = {
   MALFORMED_INGREDIENT_DEFINITION: "malformed_unit_definition",
 };
 
-function success(quantity, name) {
-  return {
-    success: true,
-    result: {
-      quantity: quantity,
-      unit: "g",
-      name: name,
-    }
-  };
-}
-
-function failure(errors) {
-  return {
-    success: false,
-    errors: errors,
-  };
-}
-
 function convertIngredient(ingredient, unit_definitions, ingredient_definitions) {
   var resolvedUnit = unit_definitions.find((unit_defintion) => {
     return unit_defintion.unit === ingredient.unit;
@@ -38,50 +20,54 @@ function convertIngredient(ingredient, unit_definitions, ingredient_definitions)
     return ingredient_definition.name === ingredient.name;
   });
 
-  if (resolvedUnit && resolvedIngredient) {
-    var errors = [];
-    if (typeof resolvedUnit.mL !== 'number') {
-      errors.push(convertIngredientError.MALFORMED_UNIT_DEFINITION);
-    }
+  var errors = [];
 
-    if (typeof resolvedIngredient.density !== 'number') {
-      errors.push(convertIngredientError.MALFORMED_INGREDIENT_DEFINITION);
-    }
-    var quantityFraction;
-    try {
-      quantityFraction = Fraction(ingredient.quantity);
-    } catch (e) {
-      if (e instanceof Fraction.DivisionByZero) {
-        errors.push(convertIngredientError.DIVIDE_BY_ZERO);
-      } else if (e instanceof Fraction.InvalidParameter) {
-        errors.push(convertIngredientError.MALFORMED_QUANTITY);
-      } else {
-        throw e;
-      }
-    }
-
-    if (errors.length > 0) {
-      return failure(errors);
-    }
-
-    var convertedQuantity = quantityFraction
-      .mul(resolvedIngredient.density)
-      .mul(resolvedUnit.mL)
-      .round()
-      .toString();
-
-    return success(convertedQuantity, resolvedIngredient.name);
-  } else {
-    var errors = [];
-    if (!resolvedUnit) {
-      errors.push(convertIngredientError.UNRECOGNIZED_UNIT);
-    }
-    if (!resolvedIngredient) {
-      errors.push(convertIngredientError.UNRECOGNIZED_INGREDIENT);
-    }
-
-    return failure(errors);
+  if (!resolvedUnit) {
+    errors.push(convertIngredientError.UNRECOGNIZED_UNIT);
+  } else if (typeof resolvedUnit.mL !== 'number') {
+    errors.push(convertIngredientError.MALFORMED_UNIT_DEFINITION);
   }
+
+  if (!resolvedIngredient) {
+    errors.push(convertIngredientError.UNRECOGNIZED_INGREDIENT);
+  } else if (typeof resolvedIngredient.density !== 'number') {
+    errors.push(convertIngredientError.MALFORMED_INGREDIENT_DEFINITION);
+  }
+
+  var quantityFraction;
+  try {
+    quantityFraction = Fraction(ingredient.quantity);
+  } catch (e) {
+    if (e instanceof Fraction.DivisionByZero) {
+      errors.push(convertIngredientError.DIVIDE_BY_ZERO);
+    } else if (e instanceof Fraction.InvalidParameter) {
+      errors.push(convertIngredientError.MALFORMED_QUANTITY);
+    } else {
+      throw e;
+    }
+  }
+
+  if (errors.length > 0) {
+    return {
+      success: false,
+      errors: errors,
+    };
+  }
+
+  var convertedQuantity = quantityFraction
+    .mul(resolvedIngredient.density)
+    .mul(resolvedUnit.mL)
+    .round()
+    .toString();
+
+  return {
+    success: true,
+    result: {
+      quantity: convertedQuantity,
+      unit: "g",
+      name: resolvedIngredient.name,
+    }
+  };
 }
 
 module.exports = [
