836eb836a42f2dad2d4fe2d8506dd5cee8233a2b
Max Holder <mxhold@gmail.com>
2018-11-03 14:55:54 -0700

    Use domain for parsing and conversion

--- reconv/src/IngredientList.js
+++ reconv/src/IngredientList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Ingredient, MalformedIngredient} from './Ingredient.js';
-import { parseIngredient } from 'reconv-domain';
-import convertIngredient from './convertIngredient.js';
+import { parseIngredient, convertIngredient } from 'reconv-domain';
 import "./IngredientList.css";
 
 export default function IngredientList(props) {
@@ -10,8 +9,28 @@ export default function IngredientList(props) {
   const ingredients = lines.split("\n").map((line, i) => {
     const parseResult = parseIngredient(line);
     if (parseResult.success) {
-      const ingredientData = parseResult.result;
-      const ingredient = props.convert ? convertIngredient(ingredientData) : ingredientData;
+      let ingredient;
+      if (props.convert) {
+        const parsedIngredient = parseResult.result;
+        const convertResult = convertIngredient(parsedIngredient);
+        if (convertResult.success) {
+          ingredient = convertResult.result;
+        } else {
+          const errors = convertResult.errors;
+
+          ingredient = {
+            quantity: errors.quantity ? "NaN" : parsedIngredient.quantity,
+            unit: parsedIngredient.unit,
+            name: parsedIngredient.name,
+            metadata: {
+              unitFound: !errors.unit,
+              ingredientFound: !errors.ingredient,
+            }
+          }
+        }
+      } else {
+        ingredient = parseResult.result;
+      }
       return <Ingredient
         key={i}
         quantity={ingredient.quantity}
