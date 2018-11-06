0134a78bcd7ef160ae9eff457716022cded12464
Max Holder <mxhold@gmail.com>
2018-11-06 08:26:36 -0800

    Remove unused code

--- reconv/src/parseIngredient.js
+++
@@ -1,9 +0,0 @@
-export default function parseIngredient(text) {
-  const re = /^((?:\d+ )?\d+[/.]?\d*)([A-z]+) +(.+)$/;
-  const matches = re.exec(text);
-  if (matches != null) {
-    return { quantity: matches[1], unit: matches[2], name: matches[3] };
-  } else {
-    return null;
-  }
-}
