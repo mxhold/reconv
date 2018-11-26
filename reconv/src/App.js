ed256da8a901e0193249204dbd5d510f8303c585
Max Holder <mxhold@gmail.com>
2018-11-26 13:30:44 -0800

    Stop unnecessarily nesting components

--- reconv/src/App.js
+++ reconv/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import UnitDefinitionInput from './UnitDefinitionInput';
+import DefinitionInput from './DefinitionInput';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
 
     return (
       <div>
-        <UnitDefinitionInput lines={lines} ingredientDefinitions={ingredientDefinitions} unitDefinitions={unitDefinitions} />
+        <DefinitionInput lines={lines} ingredientDefinitions={ingredientDefinitions} unitDefinitions={unitDefinitions} />
       </div>
     );
   }
---
+++ reconv/src/DefinitionInput.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import IngredientListInput from './IngredientListInput.js';
+
+export default class DefinitionInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      unitDefinitions: props.unitDefinitions,
+      ingredientDefinitions: props.ingredientDefinitions,
+    };
+
+    this.handleUnitDefinitionChange = this.handleUnitDefinitionChange.bind(this);
+    this.handleIngredientDefinitionChange = this.handleIngredientDefinitionChange.bind(this);
+  }
+
+  handleUnitDefinitionChange(event) {
+    this.setState({unitDefinitions: event.target.value});
+  }
+
+  handleIngredientDefinitionChange(event) {
+    this.setState({ingredientDefinitions: event.target.value});
+  }
+
+  deserializeUnitDefinitions(string) {
+    return string.split("\n").map(line => {
+      let [unit, mL] = line.split(",");
+      mL = Number.parseFloat(mL, 10);
+      return { unit, mL };
+    });
+  }
+
+  deserializeIngredientDefinitions(string) {
+    return string.split("\n").map(line => {
+      let [name, density] = line.split(",");
+      density = Number.parseFloat(density, 10);
+      return { name, density };
+    });
+  }
+
+  render() {
+    return (
+      <div className="definition-input">
+        <div className="unit-definition-input">
+          <textarea spellCheck="false" value={this.state.unitDefinitions} onChange={this.handleUnitDefinitionChange} />
+        </div>
+
+        <div className="ingredient-definition-input">
+          <textarea spellCheck="false" value={this.state.ingredientDefinitions} onChange={this.handleIngredientDefinitionChange} />
+        </div>
+
+        <IngredientListInput
+        value={this.props.lines}
+        ingredientDefinitions={this.deserializeIngredientDefinitions(this.state.ingredientDefinitions)}
+        unitDefinitions={this.deserializeUnitDefinitions(this.state.unitDefinitions)}
+        />
+      </div>
+    );
+  }
+}
