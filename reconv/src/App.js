a66af63c7a3b6233fffa74ea59e9f91efa07c0fe
Max Holder <mxhold@gmail.com>
2018-11-26 14:45:04 -0800

    Use default defintions from domain

--- reconv/src/App.js
+++ reconv/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import DefinitionInput from './DefinitionInput';
+import { defaultIngredientDefinitions, defaultUnitDefinitions } from 'reconv-domain';
 
 class App extends Component {
   render() {
-    let ingredientDefinitions = "butter,0.9586114185\nwater,1";
-    let unitDefinitions = "cup,237\nc,237\nfloz,29.5725"
+
+    let ingredientDefinitions = defaultIngredientDefinitions.map( (ingredient) => { return ingredient.name + "," + ingredient.density }).join("\n");
+    let unitDefinitions = defaultUnitDefinitions.map( (unit) => { return unit.unit + "," + unit.mL }).join("\n");
 
     let lines = "1/2c butter\n1.2c water\n1c malk\n1x butter\n1/0c water\n1BAD_DEFN water\n1c BAD_DEFN\nnonsense";
 
