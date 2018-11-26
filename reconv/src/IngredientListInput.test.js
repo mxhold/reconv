66d66b6f87d351f7d3dccab0dde9a771aa3f1185
Max Holder <mxhold@gmail.com>
2018-11-26 13:30:51 -0800

    Fix test

--- reconv/src/IngredientListInput.test.js
+++ reconv/src/IngredientListInput.test.js
@@ -3,7 +3,9 @@ import { mount } from 'enzyme';
 import IngredientListInput from './IngredientListInput.js';
 
 it('renders a converted Ingredient for each line in the textarea', () => {
-  const wrapper = mount(<IngredientListInput value={"1/2c butter\n1c milk"} />);
+  const ingredientDefinitions = [ { name: "butter", density: 0.9586114185 } ];
+  const unitDefinitions = [ { unit: "c", mL: 237 }];
+  const wrapper = mount(<IngredientListInput value={"1/2c butter\n1c milk"} ingredientDefinitions={ingredientDefinitions} unitDefinitions={unitDefinitions} />);
   const divs = wrapper.find(".ingredient");
   const butter = divs.first();
   expect(butter.find(".ingredient-quantity").text()).toEqual("114");
