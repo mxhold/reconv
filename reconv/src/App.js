7fb5534a3c394bd24e68a4c7ff1e5f0b4a74c06c
Max Holder <mxhold@gmail.com>
2018-11-30 12:23:46 -0800

    Move definition show/hiding into container component

--- reconv/src/App.js
+++ reconv/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { defaultIngredientDefinitions, defaultUnitDefinitions } from 'reconv-domain';
-import UnitDefinitionsInput from './components/UnitDefinitionsInput';
-import IngredientDefinitionsInput from './components/IngredientDefinitionsInput';
 import IngredientListInput from './components/IngredientListInput.js';
-import styles from "./App.module.css";
+import RecipeDefinitionsInputs from './components/RecipeDefinitionsInputs';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showDefinitions: false,
       unitDefinitions: defaultUnitDefinitions,
       ingredientDefinitions: defaultIngredientDefinitions,
-    }
+    };
     this.handleUnitDefinitionsChange = this.handleUnitDefinitionsChange.bind(this);
     this.handleIngredientDefinitionsChange = this.handleIngredientDefinitionsChange.bind(this);
-    this.handleShowDefinitionsChange = this.handleShowDefinitionsChange.bind(this);
   }
 
   handleUnitDefinitionsChange(definitions) {
@@ -26,30 +22,8 @@ class App extends React.Component {
     this.setState({ingredientDefinitions: definitions})
   }
 
-  handleShowDefinitionsChange(event) {
-    this.setState({showDefinitions: event.target.checked});
-  }
-
   render() {
     let lines = "1/2c butter\n1c sugar\n2 eggs, beaten\n3 bananas, crushed\n1 1/2c flour\n1t baking soda\n1/2t salt\n1/2t vanilla";
-
-    let definitions = (
-      <div className={styles.wrapper}>
-        <div className={styles.child}>
-          <UnitDefinitionsInput
-            handleDefinitionsChange={this.handleUnitDefinitionsChange}
-            defaultDefinitions={this.state.unitDefinitions}
-          />
-        </div>
-
-        <div className={styles.child}>
-          <IngredientDefinitionsInput
-            handleDefinitionsChange={this.handleIngredientDefinitionsChange}
-            defaultDefinitions={this.state.ingredientDefinitions}
-          />
-        </div>
-      </div>
-    );
 
     return (
       <div>
@@ -62,13 +36,12 @@ class App extends React.Component {
           unitDefinitions={this.state.unitDefinitions}
         />
 
-        <div className={styles.showDefinitionsToggle}>
-          <label>
-            <input type="checkbox" value={this.state.showDefinitions} onChange={this.handleShowDefinitionsChange} />
-            Show unit/ingredient definitions
-          </label>
-        </div>
-        { this.state.showDefinitions ? definitions : null}
+        <RecipeDefinitionsInputs
+          defaultUnitDefinitions={this.state.unitDefinitions}
+          handleUnitDefinitionsChange={this.handleUnitDefinitionsChange}
+          defaultIngredientDefinitions={this.state.ingredientDefinitions}
+          handleIngredientDefinitionsChange={this.handleIngredientDefinitionsChange}
+        />
       </div>
     );
   }
---
+++ reconv/src/components/RecipeDefinitionsInputs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from "./RecipeDefinitionsInputs.module.css";
+import UnitDefinitionsInput from './UnitDefinitionsInput';
+import IngredientDefinitionsInput from './IngredientDefinitionsInput';
+
+export default class RecipeDefinitionsInputs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDefinitions: false,
+    };
+    this.handleShowDefinitionsChange = this.handleShowDefinitionsChange.bind(this);
+  }
+
+  handleShowDefinitionsChange(event) {
+    this.setState({showDefinitions: event.target.checked});
+  }
+
+  render() {
+    let definitions = (
+      <div className={styles.wrapper}>
+        <div className={styles.child}>
+          <UnitDefinitionsInput
+            handleDefinitionsChange={this.props.handleUnitDefinitionsChange}
+            defaultDefinitions={this.props.defaultUnitDefinitions}
+          />
+        </div>
+
+        <div className={styles.child}>
+          <IngredientDefinitionsInput
+            handleDefinitionsChange={this.props.handleIngredientDefinitionsChange}
+            defaultDefinitions={this.props.defaultIngredientDefinitions}
+          />
+        </div>
+      </div>
+    );
+
+    return (
+      <div>
+        <div className={styles.showDefinitionsToggle}>
+          <label>
+            <input type="checkbox" value={this.state.showDefinitions} onChange={this.handleShowDefinitionsChange} />
+            Show unit/ingredient definitions
+          </label>
+        </div>
+        { this.state.showDefinitions ? definitions : null}
+      </div>
+    );
+  }
+}
