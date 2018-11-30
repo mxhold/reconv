ce35a21aa2398c1c699850dd7efd94b581b2fe16
Max Holder <mxhold@gmail.com>
2018-11-30 14:10:15 -0800

    Refactor for clarity

--- reconv/src/components/recipe/ConvertedRecipe.js
+++ reconv/src/components/recipe/ConvertedRecipe.js
@@ -9,34 +9,34 @@ export default function ConvertedRecipe(props) {
   const ingredients = lines.split("\n").map((line, i) => {
     const parseResult = parseIngredient(line);
     if (parseResult.success) {
-      let ingredient;
       const parsedIngredient = parseResult.result;
       const convertResult = convertIngredient(parsedIngredient, props.ingredientDefinitions, props.unitDefinitions);
       if (convertResult.success) {
-        ingredient = convertResult.result;
-      } else {
-        const errors = convertResult.errors;
+        const convertedIngredient = convertResult.result;
 
-        ingredient = {
-          quantity: parsedIngredient.quantity,
-          unit: parsedIngredient.unit,
-          name: parsedIngredient.name,
-          errors: {
-            unitNotFound: errors.unit === convertIngredientError.UNRECOGNIZED,
-            ingredientNotFound: errors.ingredient === convertIngredientError.UNRECOGNIZED,
-            badQuantity: errors.quantity, // no need to distinguish between BAD_FORMAT and DIVIDE_BY_ZERO yet
-            badUnitDefinition: errors.unit === convertIngredientError.BAD_DEFINITION,
-            badIngredientDefinition: errors.ingredient === convertIngredientError.BAD_DEFINITION,
-          }
-        }
-      }
-      return <Ingredient
-        key={i}
-        quantity={ingredient.quantity}
-        unit={ingredient.unit}
-        name={ingredient.name}
-        errors={ingredient.errors}
+        return <Ingredient
+          key={i}
+          quantity={convertedIngredient.quantity}
+          unit={convertedIngredient.unit}
+          name={convertedIngredient.name}
         />;
+      } else {
+        const conversionErrors = {
+          unitNotFound: convertResult.errors.unit === convertIngredientError.UNRECOGNIZED,
+          ingredientNotFound: convertResult.errors.ingredient === convertIngredientError.UNRECOGNIZED,
+          badQuantity: convertResult.errors.quantity, // no need to distinguish between BAD_FORMAT and DIVIDE_BY_ZERO yet
+          badUnitDefinition: convertResult.errors.unit === convertIngredientError.BAD_DEFINITION,
+          badIngredientDefinition: convertResult.errors.ingredient === convertIngredientError.BAD_DEFINITION,
+        }
+
+        return <Ingredient
+          key={i}
+          quantity={parsedIngredient.quantity}
+          unit={parsedIngredient.unit}
+          name={parsedIngredient.name}
+          errors={conversionErrors}
+        />;
+      }
     } else {
       return <MalformedIngredient key={i} string={line} />;
     }
@@ -48,4 +48,3 @@ export default function ConvertedRecipe(props) {
     </div>
   );
 }
-
