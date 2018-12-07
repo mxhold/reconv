5f0020d9e2aee1952acaed37c27a8fb4954c30aa
Max Holder <mxhold@gmail.com>
2018-12-07 10:18:06 -0800

    Add built React app

---
+++ precache-manifest.3620bc405693df1cde94d55b5e4294ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9a6372047cd298682309",
+    "url": "./static/js/main.9a637204.chunk.js"
+  },
+  {
+    "revision": "29988d483748bf712bba",
+    "url": "./static/js/1.29988d48.chunk.js"
+  },
+  {
+    "revision": "9a6372047cd298682309",
+    "url": "./static/css/main.0c905339.chunk.css"
+  },
+  {
+    "revision": "c7c35f5ac77827b31e9019e793a06cd8",
+    "url": "./index.html"
+  }
+];
